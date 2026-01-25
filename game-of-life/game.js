2f72cffdb478d3d11093b701b743a5f7a0254ecb
Václav Nekolný <vaclav.nekolny50@gmail.com>
2026-01-25 20:46:51 +0100

    hsl color picker

--- game-of-life/game.js
+++ game-of-life/game.js
@@ -1,15 +1,18 @@
-// const display = document.getElementById('display-word');
 const runButton = document.querySelector('button');
 const input = document.querySelector('input');
 const textBar = document.getElementById('text-bar');
 
 const grid = document.querySelector('.grid');
 
+let color = 'hsl(180, 50%, 50%)';
+const hue = document.getElementById('hue');
+const saturation = document.getElementById('saturation');
+const lightness = document.getElementById('lightness');
+
 const GRID_W = 60;
 const GRID_H = 40;
 let pointer = [0, 20];
 drawEmptyGrid(GRID_W, GRID_H);
-allowDrawing();
 
 function createTable(x, y, fillRandomly) {
   const table = document.createElement('table');
@@ -32,7 +35,6 @@ function createTable(x, y, fillRandomly) {
 }
 
 function drawEmptyGrid(x, y) {
-  console.log('empty grid');
   const grid = document.querySelector('.grid');
 
   for (let i = 0; i < 60 * 40; i++) {
@@ -49,6 +51,18 @@ function getCell(x, y) {
   return grid.children[y * GRID_W + x];
 }
 
+function getColor() {
+  const currentColor = `hsl(${hue.value},${saturation.value}%,${lightness.value}%)`;
+
+  document.getElementById('color-picker').style.borderColor = currentColor;
+  color = currentColor;
+  document.documentElement.style.setProperty('--color', currentColor);
+  document.documentElement.style.setProperty(
+    '--color-dark',
+    `hsla(${hue.value}, 80%, 20%)`,
+  );
+}
+
 function handleKeypress(e) {
   console.log(e);
   // Render screen
@@ -59,10 +73,12 @@ function handleKeypress(e) {
   } else if (e.code == 'Space') {
     textBar.toggleAttribute('hidden');
   } else if (e.key == 'x') {
-    renderBitmapLetter('S');
-    renderBitmapLetter('T');
+    renderBitmapLetter('M');
+    renderBitmapLetter('I');
+    renderBitmapLetter('L');
+    renderBitmapLetter('K');
     renderBitmapLetter('A');
-    renderBitmapLetter('R');
+    renderBitmapLetter('T');
     renderBitmapLetter('T');
   } else {
     document.body.querySelector('table')?.remove();
@@ -77,6 +93,7 @@ function allowDrawing() {
     if (e.target.classList.contains('cell')) {
       e.target.classList.toggle('full');
     }
+    
   });
 
   grid.addEventListener('pointerover', (e) => {
@@ -85,9 +102,6 @@ function allowDrawing() {
     }
   });
 }
-
-document.addEventListener('keypress', handleKeypress);
-runButton.addEventListener('click', renderWordFromInput);
 
 async function renderWordFromInput() {
   if (!input.value) {
@@ -108,7 +122,6 @@ async function renderWordFromInput() {
   allowDrawing();
 }
 
-// DEPRECATED
 async function fetchAlphabethBitmap() {
   try {
     const res = await fetch('./monogram-bitmap.json');
@@ -133,7 +146,7 @@ async function renderBitmapLetter(letter) {
   for (let i = 0; i < letterBmp.length; i++) {
     // convert to binary
     const pixelRow = letterBmp[i].toString(2).padStart(5, '0');
-    const rowArray = pixelRow.split('').reverse(); // why I need reverse? 
+    const rowArray = pixelRow.split('').reverse(); // why I need reverse?
     for (let j = 0; j < rowArray.length; j++) {
       if (rowArray[j] == '1') {
         getCell(j + x, i + y).classList.add('full');
@@ -142,17 +155,13 @@ async function renderBitmapLetter(letter) {
   }
 
   // setting new pointer
-  pointer[0] = pointer[0] + 6
+  pointer[0] = pointer[0] + 6;
 }
 
-function renderSpace() {
-  const table = document.createElement('table');
-  table.id = 'space';
-  for (let i = 0; i < 12; i++) {
-    const tr = document.createElement('tr');
-    const td = document.createElement('td');
-    tr.appendChild(td);
-    table.appendChild(tr);
-  }
-  return table;
-}
+document.addEventListener('keypress', handleKeypress);
+runButton.addEventListener('click', renderWordFromInput);
+document.querySelectorAll('.slider').forEach((slider) => {
+  slider.addEventListener('input', getColor);
+});
+
+allowDrawing();
