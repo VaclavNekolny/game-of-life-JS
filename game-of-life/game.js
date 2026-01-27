f02c8ee5ffbd2ac277cfd7f2b7632aa42a567650
Václav Nekolný <vaclav.nekolny50@gmail.com>
2026-01-27 21:47:30 +0100

    grow/shrink color picker

--- game-of-life/game.js
+++ game-of-life/game.js
@@ -4,10 +4,13 @@ const textBar = document.getElementById('text-bar');
 
 const grid = document.querySelector('.grid');
 
+const colorPicker = document.getElementById('color-picker');
 let color = [180, 50, 50];
 const hue = document.getElementById('hue');
 const saturation = document.getElementById('saturation');
 const lightness = document.getElementById('lightness');
+const arrow = document.getElementById('arrow');
+
 let isDarkMode = false;
 let isMagicOn = false;
 
@@ -65,7 +68,7 @@ function getCell(x, y) {
 function getColor() {
   const currentColor = `hsl(${hue.value},${saturation.value}%,${lightness.value}%)`;
 
-  document.getElementById('color-picker').style.borderColor = currentColor;
+  colorPicker.style.borderColor = currentColor;
   color = [+hue.value, +saturation.value, +lightness.value];
   document.documentElement.style.setProperty('--color', currentColor);
   document.documentElement.style.setProperty(
@@ -95,10 +98,13 @@ function handleKeypress(e) {
   if (e.key === 'Escape') {
     if (textBar.classList.contains('show')) {
       textBar.classList.remove('show');
-      clearInput()
+      clearInput();
     } else {
       deselectCell();
     }
+  }
+  if (e.key === 't') {
+    growSetup();
   }
   allowDrawing();
 }
@@ -216,9 +222,9 @@ function switchMode() {
 }
 
 function clearInput() {
-        setTimeout(() => {
-      input.value = ''
-    }, 300);
+  setTimeout(() => {
+    input.value = '';
+  }, 300);
 }
 
 function handleRightClick(e) {
@@ -246,6 +252,28 @@ function selectCell([x, y]) {
   selectedCell = cell;
 }
 
+function growSetup() {
+  if (colorPicker.classList.contains('grow')) {
+    colorPicker.classList.remove('grow');
+    setArrowDown();
+  } else {
+    colorPicker.classList.add('grow');
+    setArrowUp();
+  }
+}
+
+function setArrowDown() {
+  setTimeout(() => {
+    arrow.innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M297.4 470.6C309.9 483.1 330.2 483.1 342.7 470.6L534.7 278.6C547.2 266.1 547.2 245.8 534.7 233.3C522.2 220.8 501.9 220.8 489.4 233.3L320 402.7L150.6 233.4C138.1 220.9 117.8 220.9 105.3 233.4C92.8 245.9 92.8 266.2 105.3 278.7L297.3 470.7z"/></svg>';
+  }, 200);
+}
+function setArrowUp() {
+  setTimeout(() => {
+    arrow.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M297.4 169.4C309.9 156.9 330.2 156.9 342.7 169.4L534.7 361.4C547.2 373.9 547.2 394.2 534.7 406.7C522.2 419.2 501.9 419.2 489.4 406.7L320 237.3L150.6 406.6C138.1 419.1 117.8 419.1 105.3 406.6C92.8 394.1 92.8 373.8 105.3 361.3L297.3 169.3z"/></svg>`;
+  }, 200);
+}
+
 function deselectCell() {
   if (selectedCell) {
     selectedCell.classList.remove('selected');
@@ -261,27 +289,9 @@ document.querySelectorAll('.slider').forEach((slider) => {
 document.getElementById('mode').addEventListener('click', switchMode);
 document.getElementById('clear').addEventListener('click', clearGrid);
 document.getElementById('magic').addEventListener('click', magic);
+arrow.addEventListener('click', growSetup);
 grid.addEventListener('contextmenu', handleRightClick);
 
 allowDrawing();
+setArrowDown();
 
-// DEPRECATED
-function createTable(x, y, fillRandomly) {
-  const table = document.createElement('table');
-  table.id = 'board';
-  for (let i = 0; i < x; i++) {
-    const tableRow = document.createElement('tr');
-
-    for (let j = 0; j < y; j++) {
-      const cell = document.createElement('td');
-
-      if (fillRandomly && Math.random() > 0.5) {
-        cell.classList.add('full');
-      }
-
-      tableRow.appendChild(cell);
-    }
-    table.appendChild(tableRow);
-  }
-  return table;
-}
